d33cc0fbc8efc34fb60fe3d8bcac740a20658b5a
bq-atharv-50 <atharv.mahajan@betaque.com>
2025-01-25 20:06:19 +0530

    Script.js deleted

--- script.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello");
-console.log("Enter the second line");
-console.log ("Enter the third line");
-console.log("adding fourth line");
-console.log("adding Fifth line");
-
-Adding sixth line
-Seventh line adding
-
